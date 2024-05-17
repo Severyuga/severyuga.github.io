d08b3b6af8aacb197df11267f45d0e394f12d2f0
Lana <severseveryuga@gmail.com>
2024-05-17 13:43:24 +0300

    2-3 задания

--- javascript/lab2.js
+++ javascript/lab2.js
@@ -5,3 +5,23 @@ function pow(x, n) {
         throw new Error("x и n должны быть числами, и n должно быть целым числом.");
     }
 }
+
+function sumTo(n) {
+    if (typeof n === 'number' && Number.isInteger(n) && n > 0) {
+        let sum = 0;
+        for (let i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        throw new Error("n должно быть целым положительным числом.");
+    }
+}
+
+function isLeapYear(year) {
+    if (typeof year === 'number' && Number.isInteger(year)) {
+        return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+    } else {
+        throw new Error("year должно быть целым числом.");
+    }
+}
