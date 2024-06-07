8e7e6fa9f849e9aff18a1bbc475178d0cbedf974
Lana <severseveryuga@gmail.com>
2024-06-07 10:14:58 +0300

    3 лаб

--- javascript/lab4.js
+++ javascript/lab4.js
@@ -1,63 +1,112 @@
+//задания 1,2,3
 class Book {
-    constructor(title, pubYear, price) {
+  constructor(title, pubYear, price) {
       this._title = title;
       this._pubYear = pubYear;
       this._price = price;
-    }
-  
-    // Геттеры
-    get title() {
-      return this._title;
-    }
-  
-    get pubYear() {
-      return this._pubYear;
-    }
-  
-    get price() {
-      return this._price;
-    }
-  
-    // Сеттеры
-    set title(value) {
-      if (value === '') {
-        throw new Error('Название книги не может быть пустой строкой');
-      }
-  
-      this._title = value;
-    }
-  
-    set pubYear(value) {
-      if (value <= 0) {
-        throw new Error('Год публикации должен быть положительным числом');
-      }
-  
-      this._pubYear = value;
-    }
-  
-    set price(value) {
-      if (value <= 0) {
-        throw new Error('Цена книги должна быть положительным числом');
-      }
-  
-      this._price = value;
-    }
-  
-    // Метод show
-    show() {
-      return `Название: ${this.title}, цена: ${this.price}`;
-    }
   }
-  
-  
-  // Создаем экземпляр класса Book
-  const book = new Book('Мастер и Маргарита', 1966, 500);
-  
-  // Изменяем значения свойств экземпляра класса Book
-  book.title = 'Новое название';
-  book.pubYear = 2022;
-  book.price = 1000;
-  
-  console.log(book.show()); // Название: Новое название, цена: 1000
+  get title() {
+      return this._title;
+  }
+  set title(text) {
+      if (typeof text === 'string' && text.trim() !== '')
+          this._title = text.trim();
+      else
+          console.error('Title непустая строка.');
+  }
+  get pubYear() {
+      return this._pubYear;
+  }
+  set pubYear(newPubYear) {
+      if (typeof newPubYear === 'number' && newPubYear > 0)
+          this._pubYear = newPubYear;
+      else
+          console.error('pubYear положительное число.');
+  }
+  get price() {
+      return this._price;
+  }
+  set price(newPrice) {
+      if (typeof newPrice === 'number' && newPrice > 0)
+          this._price = newPrice;
+      else
+          console.error('Price положительное число.');
+  }
+  show() {
+      console.log(`Название: ${this._title},\nГод публикации: ${this._pubYear},\nЦена: ${this._price}`);
+  }
+  static compare(book1, book2) {
+      return book1.pubYear - book2.pubYear;
+  }
+}
+let book1 = new Book('Убить пересмешника', 1960, 400);
+book1.show();
+book1.price = 400;
+book1.show();
 
-  
+console.log(book1._price);
+
+let book2 = new Book('Jane Eyre', 1847, 590);
+book2.show()
+
+let book3 = new Book('Little Women', 1868, 490);
+book3.show()
+
+let books = [book1, book2, book3];
+
+books.sort(Book.compare);
+console.log("Книги после сортировки по году издания:");
+for (let i = 0; i < books.length; ++i)
+  books[i].show();
+
+// Задание 4
+function isEmpty(_object) {
+
+  for (let key in _object)
+      return false;
+
+  if (Object.getOwnPropertySymbols(_object).length > 0)
+      return false;
+
+  return true;
+}
+let obj1 = { [Symbol()]: true };
+let obj2 = {};
+console.log("Объект 1", isEmpty(obj1));
+console.log("Объект 2", isEmpty(obj2));
+
+// Задание 5
+let classObject = {
+
+  className: "open menu",
+  addClass(cls) {
+      let classes = this.className.split(' ');
+      if (!classes.includes(cls))
+          this.className += " " + cls;
+
+      return this;
+  },
+  removeClass(cls) {
+      let classes = this.className.split(' ');
+      let index = classes.indexOf(cls);
+
+      if (index !== -1) {
+          classes.splice(index, 1);
+          this.className = classes.join(' ');
+      }
+  }
+}
+classObject.addClass('close');
+console.log(classObject.className);
+
+classObject.addClass('open');
+console.log(classObject.className);
+
+classObject.removeClass('menu');
+console.log(classObject.className);
+
+// Задание 6
+let jsonString = JSON.stringify(classObject, null, 2);
+console.log(jsonString);
+let object2 = JSON.parse(jsonString);
+console.log('Сравнение объектов из JSON:', JSON.stringify(object2) === JSON.stringify(classObject));
